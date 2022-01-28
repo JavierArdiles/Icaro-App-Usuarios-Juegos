88c59544bd8b00703dbcd6f620644720ba7000bf
Javier Ardiles <javier@amalgama.co>
2022-01-28 20:40:02 -0300

    write modificado

--- functions/write.js
+++ functions/write.js
@@ -3,3 +3,10 @@ const fs = require('fs');
 const write = (data) => fs.writeFileSync('./usuariosData.json', JSON.stringify(data, null, 2));
 
 module.exports = write
+
+
+asdasdasda
+
+asdasdasda
+asd
+asdasdasdas
